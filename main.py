3ee92dc546ec2c1ef06c326feb5df0b4715679b9
AlexKorovay <korovaysasha@icloud.com>
2026-03-19 15:28:00 +0200

    поменял отработку через функции, как по мне так удобнее

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-tack = []
+
+tasks = []
+def show_tasks(tasks):
+    if not tasks:
+        print("Список пуст")
+    else:
+        print(tasks)
+    # continue
+def tasks_append(tasks):
+    x = input("Введите задачу которую нужно добавить:")
+    tasks.append(x)
+    print(f"Успешно добавили задачу {x}")
+
+def tasks_pop(tasks):
+    print(tasks)
+    y = int(input("Введите номер задачи которую нужно удалить:"))
+    tasks.pop(y - 1)
+    if not tasks:
+        print("Список пуст")
+    else:
+        print(tasks)
+
 while True:
     x = int(input("1. Показать список\n"
                   "2. Добавить задачу\n"
@@ -7,28 +28,12 @@ while True:
                   "Введите действие:\n"))
     match x:
         case 1:
-            if not tack:
-                print("Список пуст")
-            else:
-                print(tack)
-            continue
-
-
+            show_tasks(tasks)
         case 2:
-            x = input("Введите задачу которую нужно добавить:")
-            tack.append(x)
-            print(f"Успешно добавили задачу {x}")
-
+            tasks_append(tasks)
         case 3:
-            print(tack)
-            y = int(input("Введите номер задачи которую нужно удалить:"))
-            tack.pop(y-1)
-            if not tack:
-                print("Список пуст")
-            else:
-                print(tack)
-
+            tasks_pop(tasks)
         case 4:
-
             break
+
 
